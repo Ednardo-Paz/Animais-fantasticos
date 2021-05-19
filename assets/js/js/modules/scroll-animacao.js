d61b569fc25825b9dd2710c32399fafc5b0430f7
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-05-19 09:32:19 -0300

    up
    up

---
+++ assets/js/js/modules/scroll-animacao.js
@@ -0,0 +1,22 @@
+export default function initAnimeScroll() {
+  const sections = document.querySelectorAll(`[data-anime="scroll"]`);
+  if (sections.length) {
+    const metadeTela = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((item) => {
+        const sec = item.getBoundingClientRect().top;
+        const isSectionVisible = sec - metadeTela < 0;
+        if (isSectionVisible) {
+          item.classList.add(`ativo`);
+        } else 
+          item.classList.remove(`ativo`);
+        
+      });
+    }
+    animaScroll();
+
+    window.addEventListener(`scroll`, animaScroll);
+  }
+}
+
---
+++ assets/js/js/novoscript.js
@@ -0,0 +1,9 @@
+
+import initScrollSuave from `../js/modules/scroll-susave.js`
+import initAnimeScroll from `../js/modules/scroll-animacao.js`
+
+
+initScrollSuave();
+initAnimeScroll();
+
+
