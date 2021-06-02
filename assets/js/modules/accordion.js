02f65f72f318d094513a948df22b6732764a0d62
Ednardo Paz <eds_paz@hotmail.com>
2021-06-01 23:00:10 -0300

    Refatorados Accordion, TabNav.
    Refatorados Accordion, TabNav.

--- assets/js/modules/accordion.js
+++ assets/js/modules/accordion.js
@@ -1,18 +1,27 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll(
-    `[data-anime="accordion"] dt`,
-  );
-  const active = `ativo`;
-  accordionList[0].classList.add(active);
-  accordionList[0].nextElementSibling.classList.add(active);
-  function activeAccordion() {
-    this.classList.toggle(active);
-    this.nextElementSibling.classList.toggle(active);
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = 'ativo';
   }
 
-  if (accordionList.length) {
-    accordionList.forEach((item) => {
-      item.addEventListener(`click`, activeAccordion);
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
+
+  // adiciona os eventos ao accordion
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener('click', () => this.toggleAccordion(item));
     });
+  }
+
+  // iniciar função
+  init() {
+    if (this.accordionList.length) {
+      // ativar primeiro item
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
   }
 }
--- assets/js/novoscript.js
+++ assets/js/novoscript.js
@@ -1,7 +1,7 @@
 import initAnimeScroll from "./modules/scroll-animacao.js";
 import ScrollSuave from "./modules/scroll-suave.js";
-import initAccordion from "./modules/accordion.js";
-import initTabNav from "./modules/tabnav.js";
+import Accordion from "./modules/accordion.js";
+import TabNav from "./modules/tabnav.js";
 import initmodal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropDown from "./modules/dropdown-menu.js";
@@ -13,13 +13,19 @@ import initFetchBiticoin from "./modules/fetch-biticoin.js";
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
+tabNav.init();
+
 initFetchAnimais();
 initFuncionamento();
 initMenuMobile();
 initAnimeScroll();
 
-initAccordion();
-initTabNav();
+
+
 initmodal();
 initTooltip();
 initDropDown();
